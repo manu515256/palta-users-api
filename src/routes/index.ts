6253ee4f150ef079c65934e8b0078cffec0d0590
Manuel Doña <57815354+manu515256@users.noreply.github.com>
2021-03-12 12:22:45 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import routerx from 'express-promise-router';
 import userRouter from './user';
+import childRouter from './childs';
+
 const router = routerx();
 
 router.use('/user', userRouter);
+router.use('/childs', childRouter);
 
 export default router;
